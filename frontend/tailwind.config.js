5fb7ec2f15de581aef0003f28d6d5f8b3870eef1
Valentin Manes <valentinmanes@outlook.fr>
2022-09-17 15:15:11 +0200

    Final daisy fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./sections/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     darkTheme: "pastel",
