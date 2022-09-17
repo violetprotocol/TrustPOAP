43f6c6ea526734a437beef8249626da671daf30f
Gabriel Ferraz <sudoferraz@gmail.com>
2022-09-17 19:02:58 +0200

    Adds leave a review card

--- frontend/src/components/leaveReviewCard.tsx
+++ frontend/src/components/leaveReviewCard.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 export const LeaveReviewCard = () => {
 
   return (
-    <div> Test Leave Review Card </div>
+    <div className="card lg:card-bottom bg-base-100 shadow-xl max-w-2xl">
+      <div className="card-body">
+        <div className="flex justify-between">
+          <a className="link" href={""}>Leave your review</a>
+          <div className="rating gap-1">
+            <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" checked={false}/>
+            <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked={false}/>
+            <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" checked={false}/>
+            <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" checked={false}/>
+            <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" checked={false}/>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+      <a className="pl-8"> Sort By: </a>
+      <a className="font-bold pl-1"> Review Date </a>
+      </div>
+    </div>
   );
 };
 
