0066c7f0ab19a311217e989f0ad521ed95cd8b75
Valentin Manes <valentinmanes@outlook.fr>
2022-09-17 14:58:38 +0200

    Move frontend folder

--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    darkTheme: "pastel",
-  },
-};
