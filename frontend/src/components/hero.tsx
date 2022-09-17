28e8d74ec7cc56b211a785a7454bfd697ff94edc
Gabriel Ferraz <sudoferraz@gmail.com>
2022-09-17 14:08:25 +0200

    Adds gitpoapApiClient

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,4 +1,7 @@
+import { useAccount } from "wagmi";
+
 export const Hero = () => {
+const { address } = useAccount();
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
@@ -7,7 +10,9 @@ export const Hero = () => {
           <p className="py-8 text-lg">
             {"Humanbound Token + POAP => 1 Review allowed"}
           </p>
-          <button className="btn mt-5 btn-lg">Review</button>
+          <button className="btn mt-5 btn-lg"
+          disabled={!address}
+          >Search Reviews</button>
         </div>
       </div>
     </div>
