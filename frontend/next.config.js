c08eb136650a3abfff85a790995715b5d410b5e5
Gabriel Ferraz <sudoferraz@gmail.com>
2022-09-17 21:43:45 +0200

    Adds dynamic event loading

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    domains: ["assets.poap.xyz"],
     remotePatterns: [
       {
         protocol: "https",
--- frontend/src/pages/event.tsx
+++ frontend/src/pages/event.tsx
@@ -1,8 +1,29 @@
+import { useContext } from "react";
 import EventCard from "../components/EventCard/eventCard";
 import LeaveReviewCard from "../components/leaveReviewCard";
 import ReviewCard from "../components/reviewCard";
+import { UserTokensContext } from "../context/userTokens";
 
 export const EventPage = () => {
+  const ctx = useContext(UserTokensContext);
+  
+  if (ctx.event) {
+  return (
+    <div>
+      <EventCard
+        name={ctx.event.name}
+        link={ctx.event.event_url}
+        description={ctx.event.description}
+        startDate={ctx.event.start_date}
+        location={ctx.event.city}
+        imageUrl={ctx.event.image_url}
+      />
+      <LeaveReviewCard />
+      <ReviewCard />
+    </div>
+  );
+  }
+
   return (
     <div>
       <EventCard
