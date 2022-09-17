3f0c7871616bc3b455ea1b0285caa63ea016184a
Gabriel Ferraz <sudoferraz@gmail.com>
2022-09-17 19:22:15 +0200

    Adds correct parsed values on review Card

--- frontend/src/components/reviewCard.tsx
+++ frontend/src/components/reviewCard.tsx
@@ -15,24 +15,29 @@ export const ReviewCard: FC<ReviewCardProps> = ({
   reviewBody,
   reviewDate,
 }) => {
+  const reviewerAddressParsed = reviewerAddress ? reviewerAddress : "sudoferraz.eth";
+  const reviewScoreParsed = reviewScore ? reviewScore : 3;
+  const reviewTitleParsed = reviewTitle ? reviewTitle : "I cannot believe this event was free!";
+  const reviewBodyParsed = reviewBody ? reviewBody : "This was my first time in Berlin and I had 6 different doughnuts. Food is much better than back in my days in Nam. I was expecting to see Vitalik though.";
+  const reviewDateParsed = reviewDate ? reviewDate : "Sept. 21 2022";
+
 
   return (
     <div className="card lg:card-bottom bg-base-100 shadow-xl max-w-2xl">
       <div className="card-body">
             <div className="flex justify-between">
-            <span className="badge">sudoferraz.eth</span>
+            <span className="badge">{reviewerAddressParsed}</span>
               <div className="rating gap-1">
-                <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" checked={true}/>
-                <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked={false}/>
-                <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" checked={false}/>
-                <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" checked={false}/>
-                <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" checked={false}/>
+                <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" checked={reviewScoreParsed >= 1}/>
+                <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked={reviewScoreParsed >= 2}/>
+                <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" checked={reviewScoreParsed >= 3}/>
+                <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" checked={reviewScoreParsed >= 4}/>
+                <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" checked={reviewScoreParsed >= 5}/>
               </div>
             </div>
-        <h2 className="card-title pt-3">I cannot believe this event was free!</h2>
+        <h2 className="card-title pt-3">{reviewTitleParsed}</h2>
         <p>
-          This was my first time in Berlin and I had 6 different doughnuts. Food is much better than back in my days in Nam.
-          I was expecting to see Vitalik though.
+          {reviewBodyParsed}
         </p>
       </div>
       <div className="divider"></div>
@@ -42,7 +47,7 @@ export const ReviewCard: FC<ReviewCardProps> = ({
           <div className="pl-5 pb-5"> 👎 Not useful </div>
         </div>
         <div>
-          <a className="pr-5"> Sept. 21 2022 </a>
+          <a className="pr-5">{reviewDateParsed}</a>
         </div>
       </div>
     </div>
