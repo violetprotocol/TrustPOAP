7ec09e108b5c12f1be74fd54bfb5e47478854007
Valentin Manes <valentinmanes@outlook.fr>
2022-09-17 17:11:26 +0200

    Form page

---
+++ frontend/src/pages/form.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import { ReviewForm } from "../../components/reviewForm/form";
+
+const Form: NextPage = () => {
+  return (
+    <div className="min-h-screen text-center bg-base-200">
+      <div className="p-9 max-w-lg">
+        <article className="prose mb-5">
+          <h1>Form Test</h1>
+        </article>
+
+        <ReviewForm />
+      </div>
+    </div>
+  );
+};
+
+export default Form;
