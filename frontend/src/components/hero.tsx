d89ab41d30ad329656abe41aff536c5b9076c5a5
Valentin Manes <valentinmanes@outlook.fr>
2022-09-17 10:30:34 +0200

    More unused code removal

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -2,14 +2,12 @@ export const Hero = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+        <div className="max-w-lg">
+          <h1 className="text-5xl font-bold">Review Web3 Events</h1>
+          <p className="py-8 text-lg">
+            {"Humanbound Token + POAP => 1 Review allowed"}
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary mt-5 btn-lg">Review</button>
         </div>
       </div>
     </div>
