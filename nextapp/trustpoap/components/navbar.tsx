e96c6d317c6de5db03d46edeb0c4c56cefd14260
Gabriel Ferraz <sudoferraz@gmail.com>
2022-09-17 14:51:02 +0200

    Adds components to nextjs app

---
+++ nextapp/trustpoap/components/navbar.tsx
@@ -0,0 +1,36 @@
+import { ConnectButton, useConnectModal } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
+import { useAccount } from "wagmi";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
+          TrustPOAP
+        </Link>
+      </div>
+      <div className="flex-none">
+        <Rainbowbutton />
+      </div>
+    </div>
+  );
+};
+
+const Rainbowbutton = () => {
+  const { isConnected } = useAccount();
+  const { openConnectModal } = useConnectModal();
+
+  if (isConnected)
+    return <ConnectButton accountStatus="address" showBalance={false} />;
+
+  return (
+    <button
+      onClick={openConnectModal}
+      className="btn btn-primary text-base 2xl:text-lg normal-case bg-black rounded"
+    >
+      Connect Wallet
+    </button>
+  );
+};
--- nextapp/trustpoap/pages/_app.tsx
+++ nextapp/trustpoap/pages/_app.tsx
@@ -2,6 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect, useState } from 'react';
 import WebThreeProvider from "../context/web3Provider";
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiConfig } from 'wagmi';
+import { Navbar } from '../components/navbar';
+import { UserTokenProvider } from '../context/userTokens';
+import { Hero } from '../components/hero';
 
 
 const useIsMounted = (): boolean => {
@@ -19,7 +24,23 @@ function MyApp({ Component, pageProps }: AppProps) {
   if (!isMounted || !provider) return null;
 
   const { chains, wagmiClient } = provider;
-  return <Component {...pageProps} />
+  return (
+    <div className="App">
+      <header>
+        <title>Humanbound</title>
+        <meta name="description" content="Trust POAP - from Violet" />
+        <link rel="shortcut icon" href="/violet.svg" />
+      </header>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <UserTokenProvider>
+            <Navbar />
+            <Hero />
+          </UserTokenProvider>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </div>
+  );
 }
 
 export default MyApp
