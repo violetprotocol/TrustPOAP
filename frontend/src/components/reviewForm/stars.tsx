9f1b56faeda8df9f180379cdd80d06c591a409ac
Papa Smurf <chris@violet.co>
2022-09-18 05:09:17 +0200

    coloured stars

--- frontend/src/components/reviewForm/stars.tsx
+++ frontend/src/components/reviewForm/stars.tsx
@@ -3,12 +3,27 @@ import { useState } from "react";
 export const Stars = ({ register }) => {
   const [rating, setRating] = useState<number>(2);
 
+  const colour = () => {
+    switch (rating) {
+      case 1:
+        return "bg-red-400";
+      case 2:
+        return "bg-orange-400";
+      case 3:
+        return "bg-yellow-400";
+      case 4:
+        return "bg-lime-400";
+      case 5:
+        return "bg-green-400";
+    }
+  };
+
   return (
     <div className="rating gap-1 rating-lg">
       <input
         type="radio"
         name="rating-3"
-        className="mask mask-star-2 bg-accent"
+        className={`mask mask-star-2 ${colour()}`}
         onClick={() => setRating(1)}
         checked={rating === 1}
         value={1}
@@ -17,7 +32,7 @@ export const Stars = ({ register }) => {
       <input
         type="radio"
         name="rating-3"
-        className="mask mask-star-2 bg-accent"
+        className={`mask mask-star-2 ${colour()}`}
         onClick={() => setRating(2)}
         checked={rating === 2}
         value={2}
@@ -26,7 +41,7 @@ export const Stars = ({ register }) => {
       <input
         type="radio"
         name="rating-3"
-        className="mask mask-star-2 bg-accent"
+        className={`mask mask-star-2 ${colour()}`}
         onClick={() => setRating(3)}
         checked={rating === 3}
         value={3}
@@ -35,7 +50,7 @@ export const Stars = ({ register }) => {
       <input
         type="radio"
         name="rating-3"
-        className="mask mask-star-2 bg-accent"
+        className={`mask mask-star-2 ${colour()}`}
         onClick={() => setRating(4)}
         checked={rating === 4}
         value={4}
@@ -44,7 +59,7 @@ export const Stars = ({ register }) => {
       <input
         type="radio"
         name="rating-3"
-        className="mask mask-star-2 bg-accent"
+        className={`mask mask-star-2 ${colour()}`}
         onClick={() => setRating(5)}
         checked={rating === 5}
         value={5}
